1d92f8567760ac949c3ff463c8eae53c8acb620e
Logan Thomas <logan.thomas005@gmail.com>
2017-10-21 15:35:26 -0500

    Modified: updated with function and name==main

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print 'hello world'
+"""
+My first python script with a function.
+"""
+
+
+def first_func():
+    """Print 'hello world'."""
+    print('hello world')
+
+
+if __name__ == '__main__':
+    first_func()
