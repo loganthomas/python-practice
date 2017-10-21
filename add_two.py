864a1b3f24eff177634fcab72c8cc949ed04b8bd
Logan Thomas <logan.thomas005@gmail.com>
2017-10-21 16:48:36 -0500

    Modified: updated function to check input type and added name==main

--- add_two.py
+++ add_two.py
@@ -1,14 +1,34 @@
-def addtwo(a,b):
-    added = a+b
+"""
+Practice Problem:
+Write a function that will take two numbers and return the sum.
+"""
+
+
+def add_two(a, b):
+    """
+    Add two numbers together.
+
+    Args:
+       a - int or float number
+       b - int or float number
+
+    Returns:
+        added - int or float sum of a and b
+    """
+    # Check both input values are either an float or integer
+    if type(a) not in (float, int):
+        raise TypeError('Enter a valid integer or float for first variable')
+    if type(b) not in (float, int):
+        raise TypeError('Enter a valid float or integer for second variable')
+
+    # Add two given values together
+    added = a + b
+
     return added
 
-x = raw_input('Provide x:')
-y = raw_input('Provide y:')
 
-try:
-    x=float(x)
-    y=float(y)
-except:
-    print 'Invalid Number'
-    exit()
-print addtwo(x,y)
+if __name__ == '__main__':
+    val1 = float(input('Enter value 1: '))
+    val2 = float(input('Enter value 2: '))
+    added = add_two(val1, val2)
+    print('{} plus {} is {}'.format(val1, val2, added))
