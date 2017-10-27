9ed8c1e1a030ead8047bfa7dcc436ce5201b6450
Logan Thomas <logan.thomas005@gmail.com>
2017-10-27 14:44:54 -0500

    Modified: created function with error handling

--- odd_even.py
+++ odd_even.py
@@ -1,18 +1,36 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Nov 23 22:27:57 2016
+Practice Problem:
 
-@author: thomlo02
+Ask the user for a number.
+Depending on whether the number is even or odd,
+print out an appropriate message to the user.
 """
 
-# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
 
-number = input ('Please enter a number: ')
-try:
-    num = int(number)
-    if num%2==0:
-        print(num, 'is an even number')
-    else:
-        print(num, 'is an odd number')
-except:
-    print('Please enter an integer value')
+def odd_even():
+    """
+    Ask the user for a number and print whether
+    the number is odd or even.
+
+    Returns
+        print statement whether provided number is odd or even.
+    """
+    # Ask user for number
+    number = input('Please enter a number: ')
+
+    # Error handling
+    try:
+        num = int(number)
+
+        # Determine odd or even
+        if int(num) % 2 == 0:
+            print('{} is even'.format(number))
+        else:
+            print('{} is odd'.format(number))
+
+    except ValueError as err:
+        print('Value Error: {}'.format(err))
+
+
+if __name__ == '__main__':
+    odd_even()
