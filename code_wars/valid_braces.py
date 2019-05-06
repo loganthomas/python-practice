03b2f4f70a304a7b722f04117f0fcf646ded397b
loganthomas <logan.thomas005@gmail.com>
2019-05-06 09:17:06 -0500

    add valid braces exercise

---
+++ code_wars/valid_braces.py
@@ -0,0 +1,48 @@
+"""
+Write a function that takes a string of braces, and determines if the order of the braces is valid.
+It should return true if the string is valid, and false if it's invalid.
+
+This exercise is similar to the Valid Parentheses exercise, but introduces new characters:
+    brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces:
+    ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+"""
+
+
+def validBraces(string):
+    while '()' in string or '[]' in string or '{}' in string:
+        string = string.replace('()', '').replace('[]', '').replace('{}', '')
+    if string:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    """ Simple tests. Consider refactoring with pytest later. """
+    assert validBraces( '()' )             is True, "fails for '()'"
+    assert validBraces( '[]' )             is True, "fails for '[]'"
+    assert validBraces( '{}' )             is True, "fails for '{}'"
+    assert validBraces( '()' )             is True, "fails for '()'"
+    assert validBraces( '{}()[]' )         is True, "fails for '{}()[]'"
+    assert validBraces( '([{}])' )         is True, "fails for '([{}])'"
+    assert validBraces( '{}({})[]' )       is True, "fails for '{}({})[]'"
+    assert validBraces( '(({{[[]]}}))' )   is True, "fails for '(({{[[]]}}))'"
+    assert validBraces( '[(])' )           is False, "fails for '[(])'"
+    assert validBraces( '()[}[]{}' )       is False, "fails for '()[}[]{}'"
+    assert validBraces( '(([[))]]' )       is False, "fails for '(([[))]]'"
+    assert validBraces( '(([[{{}}]])){)' ) is False, "fails for '(([[{{}}]])){)'"
+
+    # Assuming asserts are never skipped/ignored
+    print('Great Success!')
+
