4552dd360fd56ac4bd879e6c8be899297b5376fd
loganthomas <logan.thomas005@gmail.com>
2019-05-09 08:02:42 -0500

    update with more efficient _is_prime func

--- code_wars/backwards_primes.py
+++ code_wars/backwards_primes.py
@@ -7,24 +7,59 @@ Examples:
 13 is such because it's prime and read from right to left writes 31 which is prime too.
 Same for the others.
 
-Task
+Task:
 Find all Backwards Read Primes between two positive given numbers (both inclusive), the second one
 always being greater than or equal to the first one.
 
 The resulting array or the resulting string will be ordered following the natural order of the
 prime numbers.
 
-Example
+Example:
 backwardsPrime(2, 100)      => [13, 17, 31, 37, 71, 73, 79, 97]
 backwardsPrime(9900, 10000) => [9923, 9931, 9941, 9967]
 backwardsPrime(501, 599)    => []
+
+Notes:
+- The original code (1 commit back) worked for a few small tests. However, was not efficient and
+  took too long to execute for larger number ranges.
+- The new solution was adopted from the below sites
+  https://www.geeksforgeeks.org/python-program-to-check-whether-a-number-is-prime-or-not/
+  https://en.wikipedia.org/wiki/Primality_test
+- A brief explanation:
+
+  We can do following optimizations:
+
+  Instead of checking till n, we can check till √n because a larger factor of n must be a multiple
+  of smaller factor that has been already checked.
+
+  The algorithm can be improved further by observing that all primes are of the form 6k ± 1,
+  with the exception of 2 and 3. This is because all integers can be expressed as (6k + i) for
+  some integer k and for i = -1, 0, 1, 2, 3, or 4;
+  2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3).
+
+  So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the
+  numbers of form 6k ± 1.
 """
 
 
-def _is_prime(x):
-    for i in range(2,x):
-        if x % i == 0:
+def _is_prime(n):
+    # Corner cases
+    if (n <= 1) :
+        return False
+    if (n <= 3) :
+        return True
+
+    # This is checked so that we can skip
+    # middle five numbers in below loop
+    if (n % 2 == 0 or n % 3 == 0) :
+        return False
+
+    i = 5
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
             return False
+        i = i + 6
+
     return True
 
 
