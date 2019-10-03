fb7a4d6d4a7a2eb03a40919f361f8358b099e6d3
loganthomas <logan.thomas005@gmail.com>
2019-10-02 22:14:44 -0500

    [TST] update with pytest for testing

--- code_wars/code_wars/friend_foe.py
+++ code_wars/code_wars/friend_foe.py
@@ -7,18 +7,11 @@ Otherwise, you can be sure he's not...
 Ex: Input = ['Ryan', 'Kieran', 'Jason', 'Yous'], Output = ['Ryan', 'Yous']
 
 Note: keep the original order of the names in the output.
+
+Test with `pytest` command (see tests dir)
 """
 
 
 def friend(x):
     return [f for f in x if len(f) == 4]
-
-
-if __name__ == '__main__':
-    """ Simple tests. Consider refactoring with pytest later. """
-    assert friend(['Ryan', 'Kieran', 'Mark',]) == ['Ryan', 'Mark'], "fails for ['Ryan', 'Kieran', 'Mark',]"
-    assert friend(['Ry', 'K', 'M',]) == [], "fails for ['Ry', 'K', 'M',]"
-
-    # Assuming asserts are never skipped/ignored
-    print('Great Success!')
 
