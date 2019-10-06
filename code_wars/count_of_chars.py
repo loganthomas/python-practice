49eb6f5201eedf92a5271a8507b78ecf0d00ec92
loganthomas <logan.thomas005@gmail.com>
2019-10-05 22:26:00 -0500

    [ENH/TST] update count_of_chars to use pytest

--- code_wars/count_of_chars.py
+++ code_wars/count_of_chars.py
@@ -11,14 +11,3 @@ from collections import Counter
 def ordered_count(_input):
     return list(Counter(_input).items())
 
-
-if __name__ == '__main__':
-    """ Simple tests. Consider refactoring with pytest later. """
-    assert ordered_count('abracadabra') == [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)], (
-        "fails for 'abracadabra'")
-    assert ordered_count('Code Wars') == [('C', 1), ('o', 1), ('d', 1), ('e', 1), (' ', 1), ('W', 1), ('a', 1), ('r', 1), ('s', 1)], (
-        "fails for 'Code Wars'")
-
-    # Assuming asserts are never skipped/ignored
-    print('Great Success!')
-
