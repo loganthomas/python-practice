6a11072966e8d19c8c50d43aefcd4a3449b2bff0
loganthomas <logan.thomas005@gmail.com>
2019-12-10 21:18:43 -0600

    [WIP] comment out day3.py still need tests

--- advent_of_code/year_2019/day3.py
+++ advent_of_code/year_2019/day3.py
@@ -116,54 +116,54 @@ def plot_wires(wire1_points, wire2_points, intersections, pt):
     plt.show()
 
 
-# Part 1 provided example 1
-wire1_points = collect_wire_points(['R8', 'U5', 'L5', 'D3'])
-wire2_points = collect_wire_points(['U7', 'R6', 'D4', 'L4'])
-wire1_metapoints = collect_wire_metapoints(wire1_points)
-wire2_metapoints = collect_wire_metapoints(wire2_points)
-intersections = find_intersections(wire1_metapoints,wire2_metapoints)
-pt, dist = find_closest_point_to_center(intersections)
+# # Part 1 provided example 1
+# wire1_points = collect_wire_points(['R8', 'U5', 'L5', 'D3'])
+# wire2_points = collect_wire_points(['U7', 'R6', 'D4', 'L4'])
+# wire1_metapoints = collect_wire_metapoints(wire1_points)
+# wire2_metapoints = collect_wire_metapoints(wire2_points)
+# intersections = find_intersections(wire1_metapoints,wire2_metapoints)
+# pt, dist = find_closest_point_to_center(intersections)
 
-plot_wires(wire1_points, wire2_points, intersections, pt)
-print(f'Point: {pt} Dist: {dist}')
-
-
-# Part 1 provided example 2
-wire1_points = collect_wire_points(['R75','D30','R83','U83','L12','D49','R71','U7','L72'])
-wire2_points = collect_wire_points(['U62','R66','U55','R34','D71','R55','D58','R83'])
-wire1_metapoints = collect_wire_metapoints(wire1_points)
-wire2_metapoints = collect_wire_metapoints(wire2_points)
-intersections = find_intersections(wire1_metapoints,wire2_metapoints)
-pt, dist = find_closest_point_to_center(intersections)
-
-plot_wires(wire1_points, wire2_points, intersections, pt)
-print(f'Point: {pt} Dist: {dist}')
-
-# Part 1 provided example 3
-wire1_points = collect_wire_points(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'])
-wire2_points = collect_wire_points(['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])
-wire1_metapoints = collect_wire_metapoints(wire1_points)
-wire2_metapoints = collect_wire_metapoints(wire2_points)
-intersections = find_intersections(wire1_metapoints,wire2_metapoints)
-pt, dist = find_closest_point_to_center(intersections)
-
-plot_wires(wire1_points, wire2_points, intersections, pt)
-print(f'Point: {pt} Dist: {dist}')
+# plot_wires(wire1_points, wire2_points, intersections, pt)
+# print(f'Point: {pt} Dist: {dist}')
 
 
-# Part 1 answer
-data_path = Path('advent_of_code/year_2019/data/day3_puzzle.txt')
-text = data_path.read_text().strip().split('\n')
-wire1_text = text[0].split(',')
-wire2_text = text[1].split(',')
+# # Part 1 provided example 2
+# wire1_points = collect_wire_points(['R75','D30','R83','U83','L12','D49','R71','U7','L72'])
+# wire2_points = collect_wire_points(['U62','R66','U55','R34','D71','R55','D58','R83'])
+# wire1_metapoints = collect_wire_metapoints(wire1_points)
+# wire2_metapoints = collect_wire_metapoints(wire2_points)
+# intersections = find_intersections(wire1_metapoints,wire2_metapoints)
+# pt, dist = find_closest_point_to_center(intersections)
 
-wire1_points = collect_wire_points(wire1_text)
-wire2_points = collect_wire_points(wire2_text)
-wire1_metapoints = collect_wire_metapoints(wire1_points)
-wire2_metapoints = collect_wire_metapoints(wire2_points)
-intersections = find_intersections(wire1_metapoints,wire2_metapoints)
-pt, dist = find_closest_point_to_center(intersections)
+# plot_wires(wire1_points, wire2_points, intersections, pt)
+# print(f'Point: {pt} Dist: {dist}')
 
-plot_wires(wire1_points, wire2_points, intersections, pt)
-print(f'Point: {pt} Dist: {dist}')
+# # Part 1 provided example 3
+# wire1_points = collect_wire_points(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'])
+# wire2_points = collect_wire_points(['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])
+# wire1_metapoints = collect_wire_metapoints(wire1_points)
+# wire2_metapoints = collect_wire_metapoints(wire2_points)
+# intersections = find_intersections(wire1_metapoints,wire2_metapoints)
+# pt, dist = find_closest_point_to_center(intersections)
+
+# plot_wires(wire1_points, wire2_points, intersections, pt)
+# print(f'Point: {pt} Dist: {dist}')
+
+
+# # Part 1 answer
+# data_path = Path('advent_of_code/year_2019/data/day3_puzzle.txt')
+# text = data_path.read_text().strip().split('\n')
+# wire1_text = text[0].split(',')
+# wire2_text = text[1].split(',')
+
+# wire1_points = collect_wire_points(wire1_text)
+# wire2_points = collect_wire_points(wire2_text)
+# wire1_metapoints = collect_wire_metapoints(wire1_points)
+# wire2_metapoints = collect_wire_metapoints(wire2_points)
+# intersections = find_intersections(wire1_metapoints,wire2_metapoints)
+# pt, dist = find_closest_point_to_center(intersections)
+
+# plot_wires(wire1_points, wire2_points, intersections, pt)
+# print(f'Point: {pt} Dist: {dist}')
 
