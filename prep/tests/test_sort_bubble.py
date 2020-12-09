981f76f6d2c89a3b2d0baead8b66f4a708e2076c
loganthomas <logan.thomas005@gmail.com>
2020-12-09 08:11:09 -0600

    [TST] initial commit of bubble sort unit tests

---
+++ prep/tests/test_sort_bubble.py
@@ -0,0 +1,64 @@
+# Standard libraries
+from typing import List
+
+# Third-party libraries
+import numpy as np
+import pytest
+
+# Local libraries
+from prep import sort_bubble
+
+SIMPLE_UNSORTED = [5, 4, 3, 2, 1]
+SIMPLE_SORTED = [1, 2, 3, 4, 5]
+RANDOM_UNSORTED = list(np.random.randint(0, 100, 50))
+
+
+@pytest.mark.parametrize(
+    "nums",
+    [SIMPLE_UNSORTED, SIMPLE_SORTED, RANDOM_UNSORTED],
+    ids=["simple-unsorted", "simple-sorted", "random-unsorted"],
+)
+def test_bubble_sort_naive(nums: List[int]) -> None:
+    # Setup - none necessary
+
+    # Exercise
+    result = sort_bubble.bubble_sort_naive(nums)
+
+    # Verify
+    assert sorted(nums) == result
+
+    # Cleanup - none necessary
+
+
+@pytest.mark.parametrize(
+    "nums",
+    [SIMPLE_UNSORTED, SIMPLE_SORTED, RANDOM_UNSORTED],
+    ids=["simple-unsorted", "simple-sorted", "random-unsorted"],
+)
+def test_bubble_sort(nums: List[int]) -> None:
+    # Setup - none necessary
+
+    # Exercise
+    result = sort_bubble.bubble_sort(nums)
+
+    # Verify
+    assert sorted(nums) == result
+
+    # Cleanup - none necessary
+
+
+@pytest.mark.parametrize(
+    "nums",
+    [SIMPLE_UNSORTED, SIMPLE_SORTED, RANDOM_UNSORTED],
+    ids=["simple-unsorted", "simple-sorted", "random-unsorted"],
+)
+def test_bubble_sort_early_stop(nums: List[int]) -> None:
+    # Setup - none necessary
+
+    # Exercise
+    result = sort_bubble.bubble_sort_early_stop(nums)
+
+    # Verify
+    assert sorted(nums) == result
+
+    # Cleanup - none necessary
