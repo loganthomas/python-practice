526d9e0488b0d9cc33e0b09c7a21d720219c4a23
loganthomas <logan.thomas005@gmail.com>
2020-12-09 21:01:04 -0600

    [ENH] initial commit of selection sort with unit tests

---
+++ prep/sort_selection.py
@@ -0,0 +1,29 @@
+"""
+Selection Sort
+--------------
+Simple but inefficient.
+
+Find the smallest element using a linear scan and move it to the front
+(swapping it with the front element). Then, find the second smallest and
+move it, again doing a linear scan. Continue doing this until all
+elements are in place.
+
+Runtime: O(n**2)
+Memory: O(1)
+"""
+# Standard libraries
+from typing import List
+
+
+def selection_sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums)):
+        min_index = i
+
+        # Find min
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[min_index]:
+                min_index = j
+
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+
+    return nums
