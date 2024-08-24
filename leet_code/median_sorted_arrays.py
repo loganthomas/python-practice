23bf1aa804932b5dce38375d6e9f84b53aa7c1f9
loganthomas <logan.thomas005@gmail.com>
2024-08-24 11:01:21 -0500

    DOC: add url

--- leet_code/median_sorted_arrays.py
+++ leet_code/median_sorted_arrays.py
@@ -1,4 +1,5 @@
 """
+See: https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 Given two sorted arrays nums1 and nums2 of size m and n respectively,
 return the median of the two sorted arrays.
 
