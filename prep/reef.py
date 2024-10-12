88ff280c92323d56a397ac7141949c9c98ba370c
loganthomas <logan.thomas005@gmail.com>
2024-10-12 18:52:19 -0500

    CLN: update reef approach

--- prep/reef.py
+++ prep/reef.py
@@ -21,61 +21,49 @@ import csv
 import io
 import sys
 
+###############################
+# Original Interview Solution #
+###############################
+# def swap_first_second_cols(row):
+#     # Handle double quotes parsing from csv
+#     if (row[1].startswith('"')) & (not row[1].endswith('"')):
+#         row[1] = ','.join([row[1], row[2]])
+#         row.pop(2)
+#     row[0], row[1] = row[1], row[0]
+#     return row
 
-def swap_first_second_cols(row):
-    # Handle double quotes parsing from csv
-    if (row[1].startswith('"')) & (not row[1].endswith('"')):
-        row[1] = ','.join([row[1], row[2]])
-        row.pop(2)
-    row[0], row[1] = row[1], row[0]
-    return row
+
+# def main():
+#     data = sys.stdin.readlines()
+
+#     for row in csv.reader(data, quotechar="'"):
+#         swapped = swap_first_second_cols(row)
+#         print(','.join(swapped))
+###############################
 
 
-def main():
-    data = sys.stdin.readlines()
+def swap_cols():
+    reader = csv.reader(sys.stdin, quotechar='"')
+    writer = csv.writer(sys.stdout)
 
-    for row in csv.reader(data, quotechar="'"):
-        swapped = swap_first_second_cols(row)
-        print(','.join(swapped))
+    for row in reader:
+        row[0], row[1] = row[1], row[0]
+        writer.writerow(row)
 
 
 if __name__ == '__main__':
-    # Setup problem 1
-    sys.stdin = io.StringIO('1,2,3\na,b,c')
-    print('Input:')
-    for line in sys.stdin.readlines():
-        print(line.strip())
-    print('\nOutput:')
-    sys.stdin = io.StringIO('1,2,3\na,b,c')
-    main()
+    print('Problem 1')
+    given = '1,2,3\na,b,c'
+    print('Given:')
+    print(given)
+    print('Output:')
+    sys.stdin = io.StringIO(given)
+    swap_cols()
     print()
-
-    # Setup problem 2
-    sys.stdin = io.StringIO('1,"2,3",4')
-    print('Input:')
-    for line in sys.stdin.readlines():
-        print(line.strip())
-    print('\nOutput:')
-    sys.stdin = io.StringIO('1,"2,3",4')
-    main()
-    print()
-
-    # Unique Case...
-    sys.stdin = io.StringIO('1,"2",3')
-    print('Input:')
-    for line in sys.stdin.readlines():
-        print(line.strip())
-    print('\nOutput:')
-    sys.stdin = io.StringIO('1,"2",3')
-    main()
-    print()
-
-    # Unique Case...
-    sys.stdin = io.StringIO('1,"2",3\n4,"5,6","7",8,"9"')
-    print('Input:')
-    for line in sys.stdin.readlines():
-        print(line.strip())
-    print('\nOutput:')
-    sys.stdin = io.StringIO('1,"2",3\n4,"5,6","7",8,"9"')
-    main()
-    print()
+    print('Problem 2')
+    given = '1,"2,3",4'
+    print('Given:')
+    print(given)
+    print('Output:')
+    sys.stdin = io.StringIO(given)
+    swap_cols()
